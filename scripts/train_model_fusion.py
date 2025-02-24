ea323e7a4673ca11ef95df3f0590a2d0ce6d59ed
dmitrdv <dmitrdv@gmail.com>
2025-02-24 17:09:16 +0500

    first version of fusion model

--- scripts/train_model_fusion.py
+++ scripts/train_model_fusion.py
@@ -3,9 +3,9 @@ import numpy as np
 import pandas as pd
 import tensorflow as tf
 from matplotlib import pyplot as plt
-from sklearn.metrics import mean_absolute_error, mean_squared_error
-from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler, RobustScaler, StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_absolute_error, mean_squared_error
 
 def prepare_features_kline(df):
   # Define columns for scale 
@@ -64,7 +64,7 @@ def prepare_features_order_book(df):
   columns_spread_relative = ['relative_spread_mean', 'relative_spread_std']
   columns_volume = ['total_best_ask_volume_mean', 'total_best_ask_volume_std', 'total_best_bid_volume_mean', 'total_best_bid_volume_std']
   columns_volume_max = ['total_best_ask_volume_max', 'total_best_bid_volume_max']
-  columns_volume_sum = ['total_best_ask_volume_sum', 'total_best_bid_volume_sum', 'total_best_bid_volume_max']
+  columns_volume_sum = ['total_best_ask_volume_sum', 'total_best_bid_volume_sum']
   columns_volume_cumulative = ['cumulative_delta_volume_last']
   columns_volume_ratio = ['order_book_imbalance_mean', 'order_book_imbalance_std', 'volume_imbalance_ratio_mean', 'volume_imbalance_ratio_std', 'mean_ask_size_mean', 'mean_ask_size_std', 'mean_bid_size_mean', 'mean_bid_size_std', 'std_ask_size_mean', 'std_bid_size_mean']
   columns_market_depth = ['market_depth_ask_mean', 'market_depth_ask_std', 'market_depth_bid_mean', 'market_depth_bid_std']
@@ -112,83 +112,174 @@ def prepare_features_order_book(df):
   
   return df, scalers
 
+def merge_feature(df_features_kline_scaled, df_features_order_book_scaled):
+    # Add prefix
+  df_features_kline_scaled = df_features_kline_scaled.add_prefix('kline_')
+  df_features_order_book_scaled = df_features_order_book_scaled.add_prefix('order_book_')
+  
+  # Join features
+  df_features_kline_scaled['kline_startTime'] = pd.to_datetime(df_features_kline_scaled['kline_startTime'], unit='ms')
+  df_features_order_book_scaled['order_book_timestamp'] = pd.to_datetime(df_features_order_book_scaled['order_book_timestamp'], unit='ms')
+   
+  df_merged = pd.merge_asof(
+    df_features_kline_scaled.sort_values('kline_startTime'),
+    df_features_order_book_scaled.sort_values('order_book_timestamp'),
+    left_on='kline_startTime',
+    right_on='order_book_timestamp',
+    direction='backward'
+  )
+
+  min_timestamp = df_features_order_book_scaled['order_book_timestamp'].min()
+  max_timestamp = df_features_order_book_scaled['order_book_timestamp'].max()
+  df_merged = df_merged[(df_merged['kline_startTime'] >= min_timestamp) & (df_merged['kline_startTime'] <= max_timestamp)]
+
+  # Convert startTime to timestamp
+  df_merged['kline_startTime'] = df_merged['kline_startTime'].astype('int64') // 10**6
+  df_merged['order_book_timestamp'] = df_merged['order_book_timestamp'].astype('int64') // 10**6
+  
+  return df_merged
+
 if __name__ == '__main__':
+  # Load datasets
   symbol = 'BTCUSDT'
   folder_features_kline = f'features/kline/{symbol}/1'
+  folder_features_order_book = f'features/order_book/{symbol}'
 
   df_features_kline = pd.read_csv(f'{folder_features_kline}/features.csv')
+  df_features_order_book = pd.read_csv(f'{folder_features_order_book}/features.csv')
 
-  # Prepare features
+  # Prepare kline features
   df_features_kline_scaled, scalers_features_kline = prepare_features_kline(df_features_kline)
 
+  # Prepare order book features
+  df_features_order_book_scaled, scalers_features_order_book = prepare_features_order_book(df_features_order_book)
+  
+  # Merge features
+  df_features_merged = merge_feature(df_features_kline, df_features_order_book)
+  df_features_merged.to_csv(f'features/merged/{symbol}/features.csv')
+
   # Define feature columns
-  input_columns = [
-    'openPrice', 
-    'highPrice', 
-    'lowPrice', 
-    'closePrice', 
-    'volume', 
-    'turnover', 
-    'priceChange', 
-    'relativePriceChange', 
-    'logReturn', 
-    'SMA_5', 
-    'SMA_10', 
-    'EMA_5', 
-    'EMA_10',
-    'hourOfDay', 
-    'dayOfWeek', 
-    'weekOfYear', 
-    'monthOfYear', 
-    'minuteOfHour', 
-    'isWeekend', 
-    'highLowRange', 
-    'stdReturn_5m', 
-    'stdReturn_10m', 
-    'RSI_14', 
-    'MACD_line', 
-    'MACD_signal', 
-    'MACD_histogram', 
-    'Stochastic_K', 
-    'Stochastic_D', 
-    'ROC_14',
+  columns_input_kline = [
+    'kline_openPrice', 
+    'kline_highPrice', 
+    'kline_lowPrice', 
+    'kline_closePrice', 
+    'kline_volume', 
+    'kline_turnover', 
+    'kline_priceChange', 
+    'kline_relativePriceChange', 
+    'kline_logReturn', 
+    'kline_SMA_5', 
+    'kline_SMA_10', 
+    'kline_EMA_5', 
+    'kline_EMA_10',
+    'kline_hourOfDay', 
+    'kline_dayOfWeek', 
+    'kline_weekOfYear', 
+    'kline_monthOfYear', 
+    'kline_minuteOfHour', 
+    'kline_isWeekend', 
+    'kline_highLowRange', 
+    'kline_stdReturn_5m', 
+    'kline_stdReturn_10m', 
+    'kline_RSI_14', 
+    'kline_MACD_line', 
+    'kline_MACD_signal', 
+    'kline_MACD_histogram', 
+    'kline_Stochastic_K', 
+    'kline_Stochastic_D', 
+    'kline_ROC_14',
   ]
-
+  
+  columns_input_order_book = [
+    'order_book_mid_price_mean',
+    'order_book_mid_price_std',
+    'order_book_mid_price_min',
+    'order_book_mid_price_max',
+    'order_book_mid_price_last',
+    'order_book_spread_mean',
+    'order_book_spread_std',
+    'order_book_spread_max',
+    'order_book_relative_spread_mean',
+    'order_book_relative_spread_std',
+    'order_book_total_best_ask_volume_mean',
+    'order_book_total_best_ask_volume_std',
+    'order_book_total_best_ask_volume_sum',
+    'order_book_total_best_ask_volume_max',
+    'order_book_total_best_bid_volume_mean',
+    'order_book_total_best_bid_volume_std',
+    'order_book_total_best_bid_volume_sum',
+    'order_book_total_best_bid_volume_max',
+    'order_book_market_depth_ask_mean',
+    'order_book_market_depth_ask_std',
+    'order_book_market_depth_bid_mean',
+    'order_book_market_depth_bid_std',
+    'order_book_order_book_imbalance_mean',
+    'order_book_order_book_imbalance_std',
+    'order_book_vwap_ask_mean',
+    'order_book_vwap_bid_mean',
+    'order_book_vwap_total_mean',
+    'order_book_volume_imbalance_ratio_mean',
+    'order_book_volume_imbalance_ratio_std',
+    'order_book_cumulative_delta_volume_last',
+    'order_book_liquidity_pressure_ratio_mean',
+    'order_book_liquidity_pressure_ratio_std',
+    'order_book_mean_ask_size_mean',
+    'order_book_mean_ask_size_std',
+    'order_book_mean_bid_size_mean',
+    'order_book_mean_bid_size_std',
+    'order_book_std_ask_size_mean',
+    'order_book_std_bid_size_mean',
+    'order_book_realized_volatility',
+  ]
+  
   # Target variable (future relative price change)
-  target_column = 'futureClosePrice'
-
+  target_column = 'kline_futureClosePrice'
+  
   # Prepare input and output data
-  X = df_features_kline_scaled[input_columns].values # Features
-  y = df_features_kline_scaled[target_column].values # Target (future relative price change)
+  X_kline = df_features_merged[columns_input_kline].values
+  X_orderbook = df_features_merged[columns_input_order_book].values
+  y = df_features_merged[target_column].values
+  
+  # Train-Test Split
+  X_kline_train, X_kline_test, X_orderbook_train, X_orderbook_test, y_train, y_test = train_test_split(X_kline, X_orderbook, y, test_size=0.2, shuffle=False)
 
-  # Train-Test Split (80% training, 20% testing)
-  X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, shuffle=False)
+  # Reshape for LSTM
+  X_kline_train = X_kline_train.reshape(X_kline_train.shape[0], 1, X_kline_train.shape[1])
+  X_kline_test = X_kline_test.reshape(X_kline_test.shape[0], 1, X_kline_test.shape[1])
 
-  # Reshape data for LSTM (samples, time steps, features)
-  X_train = X_train.reshape(X_train.shape[0], 1, X_train.shape[1])
-  X_test = X_test.reshape(X_test.shape[0], 1, X_test.shape[1])
+  # Reshape for CNN
+  X_orderbook_train = X_orderbook_train.reshape(X_orderbook_train.shape[0], X_orderbook_train.shape[1], 1)
+  X_orderbook_test = X_orderbook_test.reshape(X_orderbook_test.shape[0], X_orderbook_test.shape[1], 1)
 
-  # Define LSTM model
-  model = tf.keras.Sequential([
-    tf.keras.layers.LSTM(128, activation='tanh', input_shape=(X_train.shape[1], X_train.shape[2]), return_sequences=False),
-    tf.keras.layers.LeakyReLU(),
-    tf.keras.layers.Dense(64, kernel_regularizer=tf.keras.regularizers.l2(0.01)),
-    tf.keras.layers.LeakyReLU(),
-    tf.keras.layers.Dense(32, kernel_regularizer=tf.keras.regularizers.l2(0.01)),
-    tf.keras.layers.LeakyReLU(),
-    tf.keras.layers.Dense(1)
-  ])
-
-  # Compile model
+  # Define LSTM model for Kline
+  input_kline = tf.keras.layers.Input(shape=(1, X_kline_train.shape[2]))
+  layer_kline = tf.keras.layers.LSTM(128, activation='tanh', return_sequences=False)(input_kline)
+  layer_kline = tf.keras.layers.Dense(64, activation='relu')(layer_kline)
+  
+  # Define CNN model for Order Book
+  input_orderbook = tf.keras.layers.Input(shape=(X_orderbook_train.shape[1], 1))
+  order_book_layer = tf.keras.layers.Conv1D(128, kernel_size=3, activation='relu')(input_orderbook)
+  order_book_layer = tf.keras.layers.MaxPooling1D(pool_size=2)(order_book_layer)
+  order_book_layer = tf.keras.layers.Flatten()(order_book_layer)
+  order_book_layer = tf.keras.layers.Dense(64, activation='relu')(order_book_layer)
+  
+  # Merge LSTM and CNN
+  merged = tf.keras.layers.concatenate([layer_kline, order_book_layer])
+  merged = tf.keras.layers.Dense(32, activation='relu')(merged)
+  output = tf.keras.layers.Dense(1)(merged)
+  
+  # Define model
   optimizer = tf.keras.optimizers.Adam(learning_rate=0.0001)
+  model = tf.keras.Model(inputs=[input_kline, input_orderbook], outputs=output)
   model.compile(optimizer=optimizer, loss='mean_squared_error')
 
   # Train model
   early_stopping = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=5, restore_best_weights=True, verbose=1)
-  model.fit(X_train, y_train, epochs=1000, batch_size=32, validation_data=(X_test, y_test), verbose=1, callbacks=[early_stopping])
+  model.fit([X_kline_train, X_orderbook_train], y_train, epochs=1000, batch_size=32, validation_data=([X_kline_test, X_orderbook_test], y_test), verbose=1, callbacks=[early_stopping])
 
   # Predictions
-  y_pred = model.predict(X_test)
+  y_pred = model.predict([X_kline_test, X_orderbook_test])
 
   # Calculate Errors
   mse = mean_squared_error(y_test, y_pred)
@@ -216,10 +307,12 @@ if __name__ == '__main__':
   print('Scaled dataset have been saved.')
 
   # Save the model and the scaler
-  model.save('models/model_kline_lstm.keras')
+  model.save('models/model_fusion.keras')
   print('Model has been saved.')
 
   # Save the feature and target scalers
   for scaler_name, scaler in scalers_features_kline.items():
-    joblib.dump(scaler, f'models/model_kline_scaler_{scaler_name}.pkl')
+    joblib.dump(scaler, f'models/model_fusion_kline_scaler_{scaler_name}.pkl')
+  for scaler_name, scaler in scalers_features_order_book.items():
+    joblib.dump(scaler, f'models/model_fusion_order_book_scaler_{scaler_name}.pkl')
   print('Scalers have been saved.')
